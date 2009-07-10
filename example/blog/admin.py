be24092b9093f081e2712311526ec0ea0eba0c11
Rob Hudson <rob@cogit8.org>
2009-07-10 06:55:21 -0700

    Adding a manytomany model to the example to test.

--- example/blog/admin.py
+++ example/blog/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
-from example.blog.models import Entry
+from example.blog.models import Entry, Tag
 from django_extensions.admin import ForeignKeyAutocompleteAdmin
+from django_extensions.admin.widgets import AdminManyToManySearchInput
 
 class EntryAdmin(ForeignKeyAutocompleteAdmin):
     related_search_fields = {
        'author': ('first_name', 'email'),
     }
+    
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        if db_field.name == 'tags':
+            kwargs['widget'] = AdminManyToManySearchInput(db_field.rel, ('name',))
+        return super(EntryAdmin, self).formfield_for_dbfield(db_field, **kwargs)
+
 admin.site.register(Entry, EntryAdmin)
+
+admin.site.register(Tag)
--- example/blog/models.py
+++ example/blog/models.py
@@ -5,6 +5,13 @@ class Entry(models.Model):
     author = models.ForeignKey(User)
     title = models.TextField()
     body = models.TextField()
+    tags = models.ManyToManyField('Tag')
 
     class Meta:
         verbose_name_plural = "entries"
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __unicode__(self):
+        return self.name
